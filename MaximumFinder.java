f713784ab6670b86971c6e14a9e91374578c1517
SaftiAshari <ashari1905107@stud.kuet.ac.bd>
2023-10-04 23:27:08 +0600

    new

--- MaximumFinder.java
+++ MaximumFinder.java
@@ -1,24 +1,18 @@
-import java.util.*;
 import javax.swing.JOptionPane;
 
-public class MaximumFinder {
-    public static int Maximum(int a, int b, int c) {
-        int max = a;
-        if (b > max) {
-            max = b;
-        }
-        if (c > max) {
-            max = c;
-        }
-        return max;
-    }
+interface Myfunction {
+    double operate(double a, double b, double c);
+}
 
+public class MaximumFinder {
     public static void main(String[] args) {
-        int a = Integer.parseInt(JOptionPane.showInputDialog("Enter first integer"));
-        int b = Integer.parseInt(JOptionPane.showInputDialog("Enter second integer"));
-        int c = Integer.parseInt(JOptionPane.showInputDialog("Enter third integer"));
-        int max = Maximum(a, b, c);
-        JOptionPane.showMessageDialog(null, "Maximum is " + max);
+        double a = Double.parseDouble(JOptionPane.showInputDialog("Enter first floating-point value:"));
+        double b = Double.parseDouble(JOptionPane.showInputDialog("Enter second floating-point value:"));
+        double c = Double.parseDouble(JOptionPane.showInputDialog("Enter third floating-point value:"));
+        Myfunction max = (x, y, z) -> Math.max(x, Math.max(y, z));
+        double z = max.operate(a, b, c);
+        String message = String.format("Maximum of %f, %f and %f is %f", a, b, c, z);
+        JOptionPane.showMessageDialog(null, message);
     }
 
 }
