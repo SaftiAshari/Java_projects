0ad0da33aacd010a264ba9336d3993b11c56aabd
SaftiAshari <ashari1905107@stud.kuet.ac.bd>
2023-10-04 23:30:14 +0600

    new

---
+++ Simple_Enum.java
@@ -0,0 +1,10 @@
+enum Day {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
+}
+
+public class Simple_Enum {
+    public static void main(String[] args) {
+        Day today = Day.MONDAY;
+        System.out.println(today);
+    }
+}
